1db07bf662f7211f19ccefb3c29f88b59e9dedaf
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2019-05-23 12:29:14 +0800

    Initial commit week3 (#3)
    * Initial commit week3
    
    * Week 3 revision
    
    * exercise3 bool function

---
+++ week3/golf.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Golf where
+
+import Data.List
+import Data.Bool
+  
+-- Exercise 1
+skips :: [a] -> [[a]]
+skips xs = [[ e | (i, e) <- zip [1..] xs, mod i n == 0] | n <- [1..length xs]]
+
+-- Exercise 2
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:rest@(y:z:_))
+    | y > x && y > z = y : localMaxima rest
+    | otherwise          = localMaxima rest
+localMaxima _ = []
+
+-- Exercise 3
+base :: String
+base = replicate 10 '=' ++ "\n0123456789\n"
+
+listToString :: [Integer] -> String
+listToString xs = map (\x -> bool ' ' '*' (elem x xs)) [0..9] ++ "\n\n"
+
+histogram :: [Integer] -> String
+histogram = (++ base) . concat . map listToString . reverse . transpose . group . sort
